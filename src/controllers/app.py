3938dffad6ca5e1ab05bac1f9db8ae54eab60cd3
EC2 Default User <ec2-user@ip-172-31-23-96.ec2.internal>
2024-12-24 08:23:18 +0000

    fix: base app for development

--- src/controllers/app.py
+++ src/controllers/app.py
@@ -2,22 +2,22 @@ from flask import Flask
 from flask_cors import *
 from .env import Env
 from ..classes.route_class import Route
-from typing import List
+# from typing import List
 
 class App:
-    def __init__(self, env: Env, routes: List[tuple[Route, any]] = None):
+    def __init__(self, env: Env, routes):
         self.env = env
         self.app = Flask(env.config.flask.name)
         self.__setup_routes__(routes) 
         self.__setup_cors__()
-    
+
     def __call__(self, *args, **kwargs):
         return self.app(*args, **kwargs)
     
     def __setup_cors__(self):
         CORS(self.app)
         
-    def __setup_routes__(self, routes: List[tuple[Route, any]]):
+    def __setup_routes__(self, routes):
         for route in routes: 
             setattr(App, route[0].name, route[1])
             self.app.add_url_rule(route[0].path, route[0].name, getattr(self, route[0].name), methods=route[0].method)
